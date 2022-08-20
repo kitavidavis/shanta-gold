616dc018969ab2311b7603f99cb5dadf318aaecb
David Kitavi <geospatialeng2017@gmail.com>
2022-08-20 13:44:56 +0300

    shanta gold

---
+++ src/App.tsx
@@ -0,0 +1,265 @@
+import { useState, useEffect } from 'react';
+import {
+  AppShell,
+  Header,
+  Footer,
+  Aside,
+  Text,
+  MediaQuery,
+  useMantineTheme,
+  Title,
+  Center,
+  SimpleGrid,
+  Paper,
+  Group,
+  RingProgress,
+  Table,
+  Anchor
+} from '@mantine/core';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useColorScheme } from '@mantine/hooks';
+import { MapContainer, TileLayer, useMap, LayersControl,  GeoJSON, Circle } from 'react-leaflet'
+import license from './geodata/license';
+import { ArrowUpRight } from 'tabler-icons-react';
+import L from "leaflet"
+import drills from './geodata/drill_colars';
+import { latLng } from 'leaflet';
+import artisanal from './geodata/artisanal_workings';
+import artisanal2 from './geodata/artisanal2';
+
+export default function App() {
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
+  const preferredColorScheme = useColorScheme();
+  const [totaldrills, setTotalDrills] = useState(drills.features.length)
+  const [totalartisanal, setTotalArtisanal] = useState(artisanal.features.length + artisanal2.features.length)
+  const [totalboundaries, setTotalBoundaries] = useState(license.features.length)
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useEffect(() => {
+    setColorScheme(preferredColorScheme)
+  }, [preferredColorScheme])
+
+  const styleDrills = () => {
+    return {
+      weight: 2,
+			opacity: 1,
+			color: 'white',
+      fillColor: '#feebe2',
+			dashArray: '3',
+			fillOpacity: 0.7,
+			interactive: true,
+      backgroundColor: 'red'
+  }
+  }
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+
+      <AppShell
+      styles={{
+        main: {
+          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
+        },
+      }}
+      navbarOffsetBreakpoint="sm"
+      asideOffsetBreakpoint="sm"
+      aside={
+        <MediaQuery smallerThan="sm" styles={{ display: 'none' }}>
+          <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+          <Text> Statistics</Text>
+            <SimpleGrid cols={1} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
+            <Paper withBorder radius="md" p="xs">
+        <Group>
+          <RingProgress
+            size={80}
+            roundCaps
+            thickness={8}
+            sections={[{ value: Math.floor((1/ 1) * 100), color: 'red' }]}
+            label={
+              <Center>
+                <ArrowUpRight size={22} />
+              </Center>
+            }
+          />
+
+          <div>
+            <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
+              Drill Colars
+            </Text>
+            <Text weight={700} size="xl">
+            {totaldrills}
+            </Text>
+          </div>
+        </Group>
+      </Paper>
+      <Paper withBorder radius="md" p="xs">
+        <Group>
+          <RingProgress
+            size={80}
+            roundCaps
+            thickness={8}
+            sections={[{ value: Math.floor((1 / 1) * 100), color: 'yellow' }]}
+            label={
+              <Center>
+                <ArrowUpRight size={22} />
+              </Center>
+            }
+          />
+
+          <div>
+            <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
+              Artisanal Workings
+            </Text>
+            <Text weight={700} size="xl">
+              {totalartisanal}
+            </Text>
+          </div>
+        </Group>
+      </Paper>
+
+      <Paper withBorder radius="md" p="xs">
+        <Group>
+          <RingProgress
+            size={80}
+            roundCaps
+            thickness={8}
+            sections={[{ value: Math.floor((1 / 1) * 100), color: 'blue' }]}
+            label={
+              <Center>
+                <ArrowUpRight size={22} />
+              </Center>
+            }
+          />
+
+          <div>
+            <Text color="dimmed" size="xs" transform="uppercase" weight={700}>
+              License Boundary
+            </Text>
+            <Text weight={700} size="xl">
+              {totalboundaries}
+            </Text>
+          </div>
+        </Group>
+      </Paper>
+
+            </SimpleGrid>
+          </Aside>
+        </MediaQuery>
+      }
+      footer={
+        <Footer height={60} p="md">
+          <Group position='apart'>
+            <Group position='left'>
+              <Text>@Shanta Gold. All rights reserved.</Text>
+            </Group>
+            <Group position='right'>
+              <Anchor href="https://geopsyresearch.org" target="_blank">Made By GeoPsy Research</Anchor>
+            </Group>
+          </Group>
+        </Footer>
+      }
+      header={
+        <Header height={70} p="md">
+          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+            <Title>Shanta Gold</Title>
+          </div>
+        </Header>
+      }
+    >
+      <MapContainer style={{height: '100%', width: '100%'}} center={[0.142, 34.66]} zoom={10} scrollWheelZoom={true}>
+      <LayersControl collapsed={false} position='topright'>
+  <LayersControl.BaseLayer checked name='OSM'>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url= "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+  />
+  </LayersControl.BaseLayer>
+  <LayersControl.BaseLayer name='CartoDB'>
+  <TileLayer
+    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+  />
+  </LayersControl.BaseLayer>
+  <LayersControl.BaseLayer name='Satellite'>
+  <TileLayer
+    attribution='&copy; Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+    url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+  />
+  </LayersControl.BaseLayer>
+        <LayersControl.Overlay checked name='Drills'>
+        <GeoJSON data={drills} onEachFeature={(f, l) => {
+            let DHID = f.properties.DHID;
+            let DHtype = f.properties.DHtype;
+            let CollarSurv = f.properties.CollarSurv;
+            let PROSPECT = f.properties.PROSPECT;
+            let TENEMENTID = f.properties.TENEMENTID;
+            l.bindPopup("<table class='table' ><tbody><tr scope='row'><td><strong>DHID</strong></td><td>"+DHID+"</td></tr><tr scope='row'><td><strong>DHtype</strong></td><td>"+DHtype+"</td></tr><tr scope='row'><td><strong>CollarSurv</strong></td><td>"+CollarSurv+"</td></tr><tr scope='row'><td><strong>Prospect</strong></td><td>"+PROSPECT+"</td></tr><tr scope='row'><td><strong>Tenement ID</strong></td><td>"+TENEMENTID+"</td></tr></tbody><table>");
+        }} pointToLayer={(f, latLng) => {
+          return new L.CircleMarker(latLng, {
+            opacity: 1,
+            weight: 2,
+            color: 'red',
+            fillColor: 'red',
+            radius: 3
+          })
+        }} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay checked name='Artisanal Workings - 1'>
+        <GeoJSON data={artisanal} onEachFeature={(f, l) => {
+        let PROSPECT = f.properties.PROSPECT;
+        let LOCAL_NAME = f.properties.LOCAL_NAME;
+        let TYPE = f.properties.TYPE;
+        let COMMENT = f.properties.COMMENT === null ? 'N/A' : f.properties.COMMENT;
+        l.bindPopup("<table class='table' ><tbody><tr scope='row'><td><strong>Prospect</strong></td><td>"+PROSPECT+"</td></tr><tr scope='row'><td><strong>Local Name</strong></td><td>"+LOCAL_NAME+"</td></tr><tr scope='row'><td><strong>Type</strong></td><td>"+TYPE+"</td></tr><tr scope='row'><td><strong>Comment</strong></td><td>"+COMMENT+"</td></tr></tbody><table>");
+
+        }} pointToLayer={(f, latLng) => {
+          return new L.CircleMarker(latLng, {
+            opacity: 1,
+            weight: 2,
+            color: 'yellow',
+            fillColor: 'yellow',
+            radius: 3
+          })
+        }} />
+
+        </LayersControl.Overlay>
+        <LayersControl.Overlay checked name="Artisanal Workings-2">
+        <GeoJSON data={artisanal2} onEachFeature={(f, l) => {
+           let PROSPECT = f.properties.PROSPECT;
+           let LOCAL_NAME = f.properties.LOCAL_NAME;
+           let TYPE = f.properties.TYPE;
+           let COMMENT = f.properties.COMMENT === null ? 'N/A' : f.properties.COMMENT;
+           l.bindPopup("<table class='table' ><tbody><tr scope='row'><td><strong>Prospect</strong></td><td>"+PROSPECT+"</td></tr><tr scope='row'><td><strong>Local Name</strong></td><td>"+LOCAL_NAME+"</td></tr><tr scope='row'><td><strong>Type</strong></td><td>"+TYPE+"</td></tr><tr scope='row'><td><strong>Comment</strong></td><td>"+COMMENT+"</td></tr></tbody><table>");
+   
+        }} pointToLayer={(f, latLng) => {
+          return new L.CircleMarker(latLng, {
+            opacity: 1,
+            weight: 2,
+            color: 'yellow',
+            fillColor: 'yellow',
+            radius: 3
+          })
+        }} />
+        </LayersControl.Overlay>
+        <LayersControl.Overlay checked name='License Boundary'>
+        <GeoJSON style={(feature) => {
+          return {
+            opacity: 1,
+            weight: 1,
+            fillOpacity: 1,
+            color: 'blue',
+            fillColor: 'blue'
+          }
+        }} data={license} />
+        </LayersControl.Overlay>
+  </LayersControl>
+</MapContainer>
+    </AppShell>
+    </MantineProvider>
+    </ColorSchemeProvider>
+  );
+}
